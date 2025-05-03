2512bd0b067fa8b35b07bb769118af80083f4c31
Brayan-724 <bjqf0724@gmail.com>
2025-05-03 11:54:45 -0700

    feat(frontend): Add modal service

---
+++ frontend/src/services/modal.ts
@@ -0,0 +1,45 @@
+import { JSX, untrack } from "solid-js";
+import SWAL, { SweetAlertOptions } from "sweetalert2";
+
+import { ThemeMode, themeMode } from "@features/theme";
+
+import styles from "./modal.module.sass";
+import { render } from "solid-js/web";
+
+const baseToast = SWAL.mixin({
+  position: "center",
+  allowOutsideClick: false,
+  showConfirmButton: false,
+  showClass: {
+    popup: styles.animation_show,
+  },
+  hideClass: {
+    popup: styles.animation_hide,
+  },
+  customClass: {
+    container: styles.container,
+    popup: styles.popup,
+  },
+});
+
+export function showModal<T = any>(
+  content: () => JSX.Element,
+  options: SweetAlertOptions = {}
+): ReturnType<typeof baseToast.fire<T>> {
+  const elem = document.createElement("div");
+  elem.style.display = "contents";
+
+  return baseToast.fire<T>({
+    theme: untrack(themeMode) === ThemeMode.System
+      ? "auto"
+      : untrack(themeMode) === ThemeMode.Dark
+      ? "dark"
+      : "light",
+    ...options,
+    html: elem,
+    didOpen() {
+      render(content, elem);
+    }
+  });
+}
+
